5002e0142ca6d67d7b8a0bef114ff8d5c0ed2b27
Grigorii Horos <horosgrisa@gmail.com>
2019-07-28 02:04:43 +0300

    Initial commit

---
+++ pr-return.plugin.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+: ${PR_PROMPT_PREFIX:=' '}
+: ${PR_PROMPT_SUFIX:=''}
+
+_pr_return() {
+  
+  local RETVAL=$?
+
+  local RETURN_SYMBOL=''
+  
+  if [[ $CLICOLOR = 1 ]]; then
+    
+    if [[ $RETVAL == 0 ]]; then
+      RETURN_SYMBOL="%{${c[green]}${c_bold}%}${ZSH_TICK}%{${c_reset}%}"
+    else
+      RETURN_SYMBOL="%{${c[red]}${c_bold}%}${ZSH_CROSS}%{${c_reset}%}"
+    fi
+    
+  else
+    
+    if [[ "$RETVAL" == 0 ]]; then
+      RETURN_SYMBOL="${ZSH_TICK}"
+    else
+      RETURN_SYMBOL="${ZSH_CROSS}"
+    fi
+    
+  fi
+  
+  pr_return="${PR_PROMPT_PREFIX}${RETURN_SYMBOL}${PR_PROMPT_SUFIX}"
+}
+
+precmd_functions+=(_pr_return)
