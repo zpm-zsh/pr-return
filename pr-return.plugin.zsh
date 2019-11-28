058679f45276fb015fc3db9abed96f8d85648cba
Grigorii Horos <horosgrisa@gmail.com>
2019-11-28 20:50:12 +0200

    Optimize

--- pr-return.plugin.zsh
+++ pr-return.plugin.zsh
@@ -6,7 +6,7 @@
 DEPENDENCES_ZSH+=( zpm-zsh/figures zpm-zsh/colors )
 
 if (( $+functions[zpm] )); then
-  zpm zpm-zsh/figures,inline zpm-zsh/colors,inline
+  zpm zpm-zsh/colors,inline
 fi
 
 _pr_return() {
@@ -14,9 +14,9 @@ _pr_return() {
   local RETURN_SYMBOL=''
   
   if [[ $RETVAL == 0 ]]; then
-    RETURN_SYMBOL="%{${c[green]}${c_bold}%}${ZSH_TICK}%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[green]}${c_bold}%}✔%{${c_reset}%}"
   else
-    RETURN_SYMBOL="%{${c[red]}${c_bold}%}${ZSH_CROSS}%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[red]}${c_bold}%}✖%{${c_reset}%}"
   fi
   
   pr_return="${PR_PROMPT_PREFIX}${RETURN_SYMBOL}${PR_PROMPT_SUFIX}"
