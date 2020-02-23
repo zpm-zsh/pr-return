6472dba1a67b8f3ed339ed7a285c78f7ae1ab7bb
Grigorii Horos <horosgrisa@gmail.com>
2020-02-23 12:28:49 +0200

    Fix vars

--- pr-return.plugin.zsh
+++ pr-return.plugin.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
-: ${PR_PROMPT_PREFIX:=' '}
-: ${PR_PROMPT_SUFIX:=''}
+typeset -g PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
+typeset -g PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
+
+typeset -g pr_return
 
 DEPENDENCES_ZSH+=( zpm-zsh/figures zpm-zsh/colors )
 
@@ -14,12 +16,12 @@ _pr_return() {
   local RETURN_SYMBOL=''
   
   if [[ $RETVAL == 0 ]]; then
-    RETURN_SYMBOL="%{${c[green]}${c_bold}%}✔%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[green]}${c_bold}%}✓%{${c_reset}%}"
   else
-    RETURN_SYMBOL="%{${c[red]}${c_bold}%}✖%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[red]}${c_bold}%}✕%{${c_reset}%}"
   fi
   
-  typeset -g pr_return="${PR_PROMPT_PREFIX}${RETURN_SYMBOL}${PR_PROMPT_SUFIX}"
+  pr_return="${PR_PROMPT_PREFIX}${RETURN_SYMBOL}${PR_PROMPT_SUFIX}"
 }
 
 precmd_functions+=(_pr_return)
