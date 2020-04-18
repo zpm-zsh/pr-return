da42442d76c2497af4aa66abf5e3faf80116afae
Grigorii Horos <horosgrisa@gmail.com>
2020-04-18 12:31:41 +0300

    Fix colors

--- pr-return.plugin.zsh
+++ pr-return.plugin.zsh
@@ -3,16 +3,16 @@
 typeset -g PR_PROMPT_PREFIX=${PR_PROMPT_PREFIX:-' '}
 typeset -g PR_PROMPT_SUFIX=${PR_PROMPT_SUFIX:-''}
 
-typeset -g pr_return="${PR_PROMPT_PREFIX}%{${c[green]}${c_bold}%}✓%{${c_reset}%}${PR_PROMPT_SUFIX}"
+typeset -g pr_return="${PR_PROMPT_PREFIX}%{${c[green]}${c[bold]}%}✓%{${c[reset]}%}${PR_PROMPT_SUFIX}"
 
 function _pr_return() {
   local RETVAL=$?
   local RETURN_SYMBOL=''
 
   if [[ $RETVAL == 0 ]]; then
-    RETURN_SYMBOL="%{${c[green]}${c_bold}%}✓%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[green]}${c[bold]}%}✓%{${c[reset]}%}"
   else
-    RETURN_SYMBOL="%{${c[red]}${c_bold}%}✕%{${c_reset}%}"
+    RETURN_SYMBOL="%{${c[red]}${c[bold]}%}✕%{${c[reset]}%}"
   fi
 
   pr_return="${PR_PROMPT_PREFIX}${RETURN_SYMBOL}${PR_PROMPT_SUFIX}"
